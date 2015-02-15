8cb1228c27c9c60f04abd2e2cabae91483ffcd2f
Luky <lukapiske@gmail.com>
2015-02-15 20:45:29 +0100

    Very simple REST communication using net/http

--- request.rb
+++ request.rb
@@ -7,11 +7,8 @@ module Szn
 
   class Request
 
-    attr_reader :method, :url, :headers, :cookies,
-                :payload, :user, :password, :read_timeout, :max_redirects,
-                :open_timeout, :raw_response, :processed_headers, :args,
-                :ssl_opts,
-                :uri
+    attr_reader :args, :method, :uri, :headers, :payload,
+                :user, :password, :processed_headers,
 
 
     def self.execute(args, & block)
@@ -22,33 +19,31 @@ module Szn
       @args     = args
       @method   = args[:method]
       @headers  = stringify_headers(args[:headers]) || {}
-      @cookies  = @headers.delete(:cookies) || args[:cookies] || {}
       @payload  = Szn::Payload.generate(args[:payload])
-      @url      = process_url_params(args[:url], headers)
-      @uri      = URI.parse(args[:url])
-
-      @user     = args[:user]
-      @password = args[:password]
+      @uri      = URI.parse(process_url_params(args[:url],headers))
       @processed_headers = make_headers headers
 
     end
 
     def execute & block
-      net_http_request_class(method)
       http = Net::HTTP.new(uri.host, uri.port)
       if uri.is_a?(URI::HTTPS)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
-
       request = net_http_request_class(method).new(uri.request_uri, headers)
-      request.basic_auth "lukapiske@gmail.com/token", "f30454af68aa70369ab46f2cf3b742ffc0b928b1"
-
+      #request.basic_auth "{email}/token", "{token}"
       response = net_http_do_request http, request, payload ? payload.to_s : nil, &block
+      binding.pry
+
 
     end
 
     private
+
+    def net_http_request_class(method)
+      Net::HTTP.const_get(method.to_s.capitalize)
+    end
 
     def net_http_do_request(http, req, body=nil, &block)
       if body != nil && body.respond_to?(:read)
@@ -57,11 +52,6 @@ module Szn
       else
         return http.request(req, body, &block)
       end
-    end
-
-
-    def net_http_request_class(method)
-      Net::HTTP.const_get(method.to_s.capitalize)
     end
 
     def process_url_params url, headers
@@ -85,27 +75,11 @@ module Szn
     def default_headers
       {
         :accept => '*/*',
-        :accept_encoding => 'gzip, deflate',
+        :accept_encoding => 'gzip, deflate'
       }
     end
 
     def make_headers user_headers
-      unless @cookies.empty?
-
-        # Validate that the cookie names and values look sane. If you really
-        # want to pass scary characters, just set the Cookie header directly.
-        # RFC6265 is actually much more restrictive than we are.
-        @cookies.each do |key, val|
-          unless valid_cookie_key?(key)
-            raise ArgumentError.new("Invalid cookie name: #{key.inspect}")
-          end
-          unless valid_cookie_value?(val)
-            raise ArgumentError.new("Invalid cookie value: #{val.inspect}")
-          end
-        end
-
-        user_headers[:cookie] = @cookies.map { |key, val| "#{key}=#{val}" }.sort.join('; ')
-      end
       headers = stringify_headers(default_headers).merge(stringify_headers(user_headers))
       headers.merge!(@payload.headers) if @payload
       headers
